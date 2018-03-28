7d3d0532ed91fc6f108b386b9b0b3c58089b1345
Josephinexuxuxu <dexing.xu@outlook.com>
2018-03-28 13:54:35 -0400

    first commit

---
+++ src/routes/Home/modules/home.js
@@ -0,0 +1,29 @@
+import update from 'react-addons-update';
+import constants from './actionConstants';
+
+const {SET_NAME} = constants;
+
+export function setName(){
+  return{
+    type: SET_NAME,
+    payload:'XDX'
+  }
+}
+
+function handleSetName(state, action){
+  return update(state, {
+    name:{
+      $set:action.payload
+    }
+  })
+}
+
+const ACTION_HANDLERS = {
+  SET_NAME: handleSetName
+}
+const initialState = {};
+export function HomeReducer (state = initalState, action){
+  const handler = ACTION_HANDLERS[action.type];
+
+  return handler ? handler(state, action):state;
+}
